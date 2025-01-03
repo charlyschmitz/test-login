3de14676ccb1daf9b44118db43353a3ea65f475b
Axel Schmitz <axelschmitz1@proton.me>
2025-01-03 17:26:21 +0100

    ... ya esta ya

--- app/components/GoogleSignIn.tsx
+++ app/components/GoogleSignIn.tsx
@@ -1,28 +1,31 @@
 // app/components/GoogleSignIn.tsx
 "use client";
+// app/components/GoogleSignIn.tsx
 import React, { useEffect } from "react";
 
 const GoogleSignIn = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
-    script.async = true;
-    script.defer = true;
-    script.onload = () => {
-      if (window.google) {
-        window.google.accounts.id.initialize({
-          client_id:
-            "219903509938-h6nvmjjaj4b457o2duau06cicua1slv8.apps.googleusercontent.com",
-          callback: handleCredentialResponse,
-        });
-        window.google.accounts.id.renderButton(
-          document.getElementById("g-signin2"),
-          { theme: "outline", size: "large" }
-        );
-        window.google.accounts.id.prompt();
-      }
-    };
-    document.body.appendChild(script);
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.src = "https://accounts.google.com/gsi/client";
+      script.async = true;
+      script.defer = true;
+      script.onload = () => {
+        if (window.google) {
+          window.google.accounts.id.initialize({
+            client_id:
+              "219903509938-h6nvmjjaj4b457o2duau06cicua1slv8.apps.googleusercontent.com",
+            callback: handleCredentialResponse,
+          });
+          window.google.accounts.id.renderButton(
+            document.getElementById("g-signin2"),
+            { theme: "outline", size: "large" }
+          );
+          window.google.accounts.id.prompt();
+        }
+      };
+      document.body.appendChild(script);
+    }
   }, []);
 
   const handleCredentialResponse = (response) => {
