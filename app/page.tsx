ccc98892cd6e21c4e101fb81e0c2c384b9a4c237
Axel Schmitz <axelschmitz1@proton.me>
2025-01-04 15:30:35 +0100

    alpha 0.3.0

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,11 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           // className="dark:invert"
-          src="/google.svg"
+          id="loginImg"
+          src="/loginCabeza.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={100}
+          height={100}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
